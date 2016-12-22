957a9d385f2ab9c188f958c08a82b31471d273b3
user <user@Ren1FHP03>
2016-12-22 10:38:41 +0800

    OOP-HW7

---
+++ src/fcu/iecs/oop/pokemon/Bulbasaur.java
@@ -0,0 +1,13 @@
+package fcu.iecs.oop.pokemon;
+
+public class Bulbasaur extends Pokemon{
+
+	public Bulbasaur(String name, PokemonType type, int cp) {
+		super(name, type, cp);
+		// TODO Auto-generated constructor stub
+	}
+	public void attack(){
+		System.out.println("Tackle...");
+	}
+
+}
